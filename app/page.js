82247cd64e4c391818c669858e7210ede8e23b64
Sebastian <sebastianfok@gmail.com>
2025-10-02 21:03:23 -0700

    added all static elements for layout

--- app/page.js
+++ app/page.js
@@ -34,14 +34,14 @@ export default function Home() {
             {/* <h1 className={styles.title}>OFFCUT</h1> */}
             <video src="/videos/ChalkAnimationWAnimeLogo.mp4" autoPlay loop />
           </div>
-          <SectionLayout minHeight='60svh'>
+          <SectionLayout minHeight='30svh'>
             <h2>info</h2>
             <div className={styles.textSection1a}>
               <p className={`${"plarge"} ${styles.indentFirstLine}`}>Offcut is a personal study of material left in its natural state, shaped but not polished. A further exploration can reveal that value isn't always in what is resolved, but in what resists being made familiar.</p>
             </div>
           </SectionLayout>
 
-          <SectionLayout minHeight='80svh'>
+          <SectionLayout >
             <div style={{ gridColumn: '1 / 4' }}>
               <MediaBlock image={rock1} alt="A stone on a white background" fit="height" />
             </div>
@@ -57,6 +57,7 @@ export default function Home() {
               <p>Lorem ipsum dolor sit amet consectetur. Pellentesque in sed sem in lectus vitae. Ipsum integer tincidunt venenatis quis. Enim ac urna nisl ullamcorper purus sollicitudin phasellus. Dictum consequat neque dui dolor consequat consequat integer eget amet. Consequat eget porttitor id at etiam vulputate in vivamus. Mauris aliquam lorem lectus arcu vitae sodales sollicitudin posuere cursus. Vestibulum volutpat commodo amet dolor. </p>
             </div>
           </SectionLayout>
+
           <SectionLayout>
             <div style={{ gridColumn: '1 / 6' }}>
               <MediaBlock image={rock2} alt="A stone on a white background" fit="height" />
@@ -67,42 +68,45 @@ export default function Home() {
             </div>
           </SectionLayout>
 
-          <SectionLayout minHeight='80svh'>
-            <div style={{ gridColumn: '1 / 2' }}>
-              <p>Lorem ipsum dolor sit amet consectetur. Enim tellus condimentum in id enim vel etiam aliquet donec. Amet eget suspendisse et in massa dolor. Enim sed netus integer donec a potenti orci. Aliquam fames amet morbi porta. Aliquam nulla condimentum eget urna morbi convallis. Quis vehicula fames lectus nisi at pulvinar in semper in. Consectetur interdum bibendum quisque quisque risus sed accumsan tortor habitasse. Suspendisse convallis nulla bibendum egestas venenatis suspendisse sit. Quis mauris arcu posuere mattis enim. Interdum eget auctor fermentum euismod at libero fames. Pretium in feugiat viverra feugiat nulla odio. </p>
-            </div>
-            <div style={{ gridColumn: '3 / 6', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-              <MediaBlock image={rock2_1} alt="A stone on a white background" fit="height" />
-              <p>1</p>
-            </div>
-          </SectionLayout>
+          <div className={styles.beakDiv}>
+            <SectionLayout>
+              <div style={{ gridColumn: '1 / 2' }}>
+                <p>Lorem ipsum dolor sit amet consectetur. Enim tellus condimentum in id enim vel etiam aliquet donec. Amet eget suspendisse et in massa dolor. Enim sed netus integer donec a potenti orci. Aliquam fames amet morbi porta. Aliquam nulla condimentum eget urna morbi convallis. Quis vehicula fames lectus nisi at pulvinar in semper in. Consectetur interdum bibendum quisque quisque risus sed accumsan tortor habitasse. Suspendisse convallis nulla bibendum egestas venenatis suspendisse sit. Quis mauris arcu posuere mattis enim. Interdum eget auctor fermentum euismod at libero fames. Pretium in feugiat viverra feugiat nulla odio. </p>
+              </div>
+              <div style={{ gridColumn: '3 / 6', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                <MediaBlock image={rock2_1} alt="A stone on a white background" fit="height" />
+                <p>1</p>
+              </div>
+            </SectionLayout>
 
-          <SectionLayout minHeight='60svh'>
-            <div style={{ gridColumn: '1 / 2' }}>
-              <p>2</p>
-              <MediaBlock image={rock2vert1} alt="A stone on a white background" fit="height" />
-            </div>
-            <div style={{ gridColumn: '2 / 3' }}>
-              <p>3</p>
-              <MediaBlock image={rock2vert4} alt="A stone on a white background" fit="height" />
-            </div>
-            <div style={{ gridColumn: '3 / 4' }}>
-              <p>4</p>
-              <MediaBlock image={rock2vert2} alt="A stone on a white background" fit="height" />
-            </div>
-            <div style={{ gridColumn: '4 / 5' }}>
-              <p>5</p>
-              <MediaBlock image={rock2vert3} alt="A stone on a white background" fit="height" />
-            </div>
-          </SectionLayout>
+            <SectionLayout>
+              <div style={{ gridColumn: '1 / 2' }}>
+                <p>2</p>
+                <MediaBlock image={rock2vert1} alt="A stone on a white background" fit="height" />
+              </div>
+              <div style={{ gridColumn: '2 / 3' }}>
+                <p>3</p>
+                <MediaBlock image={rock2vert4} alt="A stone on a white background" fit="height" />
+              </div>
+              <div style={{ gridColumn: '3 / 4' }}>
+                <p>4</p>
+                <MediaBlock image={rock2vert2} alt="A stone on a white background" fit="height" />
+              </div>
+              <div style={{ gridColumn: '4 / 5' }}>
+                <p>5</p>
+                <MediaBlock image={rock2vert3} alt="A stone on a white background" fit="height" />
+              </div>
+            </SectionLayout>
+          </div>
 
-          <SectionLayout minHeight='80svh'>
+
+          <SectionLayout>
             <div style={{ gridColumn: '1 / 4' }}>
               <p className="plarge">Lorem ipsum dolor sit amet consectetur. Iaculis potenti lobortis ornare vel. In sed libero arcu senectus et niwsl.</p>
             </div>
           </SectionLayout>
           
-          <SectionLayout minHeight='80svh'>
+          <SectionLayout>
             <div style={{ gridColumn: '1 / 4' }}>
               <MediaBlock image={rock3} alt="Rock 3. Fishbone" fit="height" />
               <h2>bone</h2>
@@ -131,23 +135,55 @@ export default function Home() {
         <section className="pageLayout" style={{ border: '1px solid red' }}>
           <SectionLayout>
             <div style={{ gridColumn: '1 / 2' }}>
-                <MediaBlock image={chalk2_1} alt="A stone on a white background" fit="height" />
+              <MediaBlock image={chalk2_1} alt="A stone on a white background" fit="height" />
             </div>
             <div style={{ gridColumn: '2 / 3' }}>
-                <MediaBlock image={chalk2_2} alt="A stone on a white background" fit="height" />
+              <MediaBlock image={chalk2_2} alt="A stone on a white background" fit="height" />
             </div>
             <div style={{ gridColumn: '3 / 4' }}>
               <p>Lorem ipsum dolor sit amet consectetur. Pellentesque in sed sem in lectus vitae. Ipsum integer tincidunt venenatis quis. Enim ac urna nisl ullamcorper purus sollicitudin phasellus. Dictum consequat neque dui dolor consequat consequat integer eget amet. Consequat eget porttitor id at etiam vulputate in vivamus. Mauris aliquam lorem lectus arcu vitae sodales sollicitudin posuere cursus. Vestibulum volutpat commodo amet dolor.</p>
             </div>
             <div style={{ gridColumn: '5 / 6' }}>
-                <MediaBlock image={chalk2_3} alt="A stone on a white background" fit="height" />
+              <MediaBlock image={chalk2_3} alt="A stone on a white background" fit="height" />
             </div>
           </SectionLayout>
 
+          <SectionLayout>
+            <div style={{ gridColumn: '4 / 6', marginBottom: 'var(--gap)' }}>
+              <p className="plarge">Lorem ipsum dolor sit amet consectetur. </p>
+            </div>
+            <div style={{ gridColumn: '2 / 6' }}>
+              <MediaBlock image={chalkLandscape2} alt="A stone on a white background" fit="height" />
+            </div>
+            <div style={{ gridColumn: '2 / 3', marginTop: 'var(--gap)' }}>
+              <p>Lorem ipsum dolor sit amet consectetur. Pellentesque in sed sem in lectus vitae. Ipsum integer tincidunt venenatis quis. Enim ac urna nisl ullamcorper purus sollicitudin phasellus. Dictum consequat neque dui dolor consequat consequat integer eget amet. Consequat eget porttitor id at etiam vulputate in vivamus. Mauris aliquam lorem lectus arcu vitae sodales sollicitudin posuere cursus. Vestibulum volutpat commodo amet dolor.</p>
+            </div>
+          </SectionLayout>
+
+          <SectionLayout>
+            <div style={{ gridColumn: '1 / 3' }}>
+              <p className="plarge">Lorem ipsum dolor sit amet consectetur. Iaculis potenti lobortis ornare vel. In sed libero arcu senectus et.</p>
+            </div>
+            <div style={{ gridColumn: '4 / 5' }}>
+              <MediaBlock image={chalk1} alt="A stone on a white background" fit="height" />
+            </div>
+          </SectionLayout>
+
+          <SectionLayout>
+            <div style={{ gridColumn: '1 / 4' }}>
+              <MediaBlock image={chalkLandscape1} alt="A stone on a white background" fit="height" />
+            </div>
+          </SectionLayout>
+
+          <SectionLayout>
+            <div style={{ gridColumn: '3 / 5' }}>
+              <p className={styles.textSection1b}>Lorem ipsum dolor sit amet consectetur. Enim tellus condimentum in id enim vel etiam aliquet donec. Amet eget suspendisse et in massa dolor. Enim sed netus integer donec a potenti orci. Aliquam fames amet morbi porta. Aliquam nulla condimentum eget urna morbi convallis. Quis vehicula fames lectus nisi at pulvinar in semper in. Consectetur interdum bibendum quisque quisque risus sed accumsan tortor habitasse. Suspendisse convallis nulla bibendum egestas venenatis suspendisse sit. Quis mauris arcu posuere mattis enim. Interdum eget auctor fermentum euismod at libero fames. Pretium in feugiat viverra feugiat nulla odio. Blandit nisl diam purus orci mauris penatibus molestie diam amet. Ipsum eget donec tellus nibh nunc dapibus neque. Leo dolor mi habitasse nibh mattis quis. Viverra fermentum mattis morbi orci ac aliquet magna id. Nec aenean id elementum urna dictumst vitae dui. Viverra amet mi mattis orci amet non velit. Etiam magna erat odio ultrices fames. Sed varius tincidunt duis ac consequat scelerisque tellus nec. Senectus pretium porttitor euismod diam etiam tristique viverra ornare a. Dignissim eu consectetur tristique tellus tempus dictum sed venenatis. Quis etiam non amet eget lacus faucibus in elit vitae. Vitae proin sagittis eu curabitur sed ornare. Pellentesque ullamcorper fusce ullamcorper pharetra fusce amet.</p>
+            </div>
+          </SectionLayout>
         </section>
       }
       bottomStripA={
-        <SectionLayout minHeight="100vh" placeItems="end">
+        <SectionLayout minHeight="100svh" placeItems="end">
           <div style={{ gridColumn: '1 / 5' }}>
               <p className="plarge">Reaching the end just means you’re close to something worth returning to.</p>
           </div>
