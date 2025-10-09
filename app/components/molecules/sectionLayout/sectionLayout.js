8ab8c18178f4ee4d92896ecad7516acbd41d2ab7
Sebastian <sebastianfok@gmail.com>
2025-10-08 22:30:14 -0700

    mobile test

--- app/components/molecules/sectionLayout/sectionLayout.js
+++ app/components/molecules/sectionLayout/sectionLayout.js
@@ -10,8 +10,8 @@ export default function SectionLayout({
  }) {
     return (
         <>
-            <div style={{height: `clamp(20svh, 45svw, ${height})`, minHeight: minHeight, display: display, placeItems: placeItems}} className={styles.sectionWrap}>
-                <div className={styles.section} style={{height: sectionHeight}}>
+            <div style={{height: `clamp(20svh, 45svw, ${height})`, minHeight: minHeight, display: display, placeItems: placeItems, border: "1px solid green"}} className={styles.sectionWrap}>
+                <div className={styles.section} style={{height: sectionHeight, border: "1px solid red"}}>
                     {children}
                 </div>
             </div>
